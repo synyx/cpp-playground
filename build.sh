e99a8c2bb4f99548523561be99df7aeb83247e11
wicked539 <sven.mueller83@gmail.com>
2014-07-26 19:28:02 +0200

    removed gtest lib :)

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# dependencies
+if [ ! -d "lib/gtest-1.7.0" ]; then
+	cd lib
+  	curl https://googletest.googlecode.com/files/gtest-1.7.0.zip -o gtest-1.7.0.zip
+  	unzip gtest-1.7.0.zip
+  	rm gtest-1.7.0.zip
+  	cd ..
+fi
+
+# clean
+rm -r build
+
+# build
+mkdir build
+cd build
+
+cmake ../
+make
+
+# run tests
+make test
