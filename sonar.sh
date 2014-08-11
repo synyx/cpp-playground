d9fc5bdab8871deae7b9a8c086d28a78110de31e
Sven Mueller <mueller@synyx.de>
2014-08-11 18:13:37 +0200

    fixed c&p error in sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -6,8 +6,8 @@ mkdir cppcheck-reports
 cppcheck -v --enable=all --xml-version=2 -I src/modules src/programm.cpp src/modules/mycalculator.cpp src/modules/mymodule.cpp 2> cppcheck-reports/cppcheck-result-all.xml
 
 #coverage
-rm -rf cppcheck-reports
-mkdir cppcheck-reports
+rm -rf coverage-reports
+mkdir coverage-reports
 cd build
 make MyModuleTest_coverage MyCalculatorTest_coverage
 cd ..
