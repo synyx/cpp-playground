b51f1abf726f39bab99ffb0b181fadcabf06c675
Sven Mueller <mueller@synyx.de>
2014-08-11 16:34:44 +0200

    added test for mycalc.multiply && mock for mymultiplier

--- test/modules/mycalculator-test.cpp
+++ test/modules/mycalculator-test.cpp
@@ -2,6 +2,10 @@
 #include "../../src/modules/mymultiplier.h"
 #include <gtest/gtest.h>
 
+double MyMultiplier::multiply(double a, double b) {
+    return 42.0;
+}
+
 TEST (addTest, AddsNumbers) { 
     MyMultiplier* myMultiplier = new MyMultiplier();
     MyCalculator* myObject = new MyCalculator(myMultiplier);
@@ -9,6 +13,15 @@ TEST (addTest, AddsNumbers) {
     double result = myObject->add(42, 1337);
 
     EXPECT_EQ (1379, result);
+}
+
+TEST (multiplyTest, MultipliesNumbers) { 
+    MyMultiplier* myMultiplier = new MyMultiplier();
+    MyCalculator* myObject = new MyCalculator(myMultiplier);
+
+    double result = myObject->multiply(3.14, 42);
+
+    ASSERT_DOUBLE_EQ (42.0, result);
 }
 
 int main(int argc, char **argv) {
